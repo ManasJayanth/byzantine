892c7a2f30fc87eaff299c0cc26fee777c9052d8
Manas <prometheansacrifice@gmail.com>
2014-04-13 01:46:01 +0530

    deply.bash updated: branching and pushing to heroku automated

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,11 @@
+git branch -D heroku-branch
+
+git checkout -b heroku-branch
+
 sed -i -e 's/mongodb:\/\/localhost\/byzantine/mongodb:\/\/rohit:rohit@ds045297.mongolab.com:45297\/byzantine/g' models/user.js
 
-
 sed -i -e 's/node post-install.js \|\| //g' package.json
+
+git commit -a -m "Deployment changes"
+
+git push heroku heroku-branch:master
