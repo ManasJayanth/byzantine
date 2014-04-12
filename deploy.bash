626eea636ac3dfd766224b027a89f97136eeab58
Manas <prometheansacrifice@gmail.com>
2014-04-13 02:22:24 +0530

    deployment.bash now merges master into heroku-branch

--- deploy.bash
+++ deploy.bash
@@ -1,11 +1,11 @@
-git branch -D heroku-branch
+git checkout heroku-branch
 
-git checkout -b heroku-branch
+git merge master -m "Deployment changes"
 
 sed -i -e 's/mongodb:\/\/localhost\/byzantine/mongodb:\/\/rohit:rohit@ds045297.mongolab.com:45297\/byzantine/g' models/user.js
 
 sed -i -e 's/node post-install.js \|\| //g' package.json
 
-git commit -a -m "Deployment changes"
+#git commit -a -m "Deployment changes"
 
 git push heroku heroku-branch:master
